31e59333eb40470b6bcd6ad6d78b89fc10608d4d
frantsuhvika@gmail.com <frantsuhvika@gmail.com>
2020-06-18 21:17:19 +0300

    added .env file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const  express = require('express')
 const app = express()
 
-const port= process.env.PORT || 7007
+const port= process.env.PORT || 3008
 
 //get pages listing
 app.get ('/', (req, res) => {
