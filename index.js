f7281acb2544c2b4309c34ae6dab7c45695e30e9
frantsuhvika@gmail.com <frantsuhvika@gmail.com>
2020-06-18 17:21:45 +0300

    added  express and get pages listing

---
+++ index.js
@@ -0,0 +1,20 @@
+//import express 
+var  express = require('express')
+var app = express()
+
+//var PORT= process.env.PORT || 80
+
+//get pages listing
+app.get ('/', (req,res)=>{
+    res.end('<div><nav><ul><li><a href="/">Home</a></li><li><a href="/register">Register</a></li></ul></nav><h1>Home page</h1></div>')
+})
+
+app.get ('/register', (req,res)=>{
+    res.end('<div><nav><ul><li><a href="/">Home</a></li><li><a href="/register">Register</a></li></ul></nav><h1>Register page</h1></div>')
+})
+
+
+
+app.listen(7004, ()=>{
+    console.log('Server started')
+})
