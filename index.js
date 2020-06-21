65a935db61937b6afec461013b3c846968bfada0
Victoria Frantsuh <frantsuhvika@gmail.com>
2020-06-21 13:02:24 +0300

    update port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const  express = require('express')
 const app = express()
 
-const port= process.env.PORT || 3008
+const port= process.env.PORT || 5000
 
 //get pages listing
 app.get ('/', (req, res) => {
